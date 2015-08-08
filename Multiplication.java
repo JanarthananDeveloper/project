ffc99e5ebac0949b36dcfa7dce902f6dba4e1fd2
JanarthananDevelopers <janarthanandevs@gmail.com>
2015-08-08 11:08:08 +0530

    my project commited

---
+++ Multiplication.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Multiplication {
+	int mul()
+	{
+		Scanner s = new Scanner(System.in);
+        System.out.println("enter the number ");
+        int no= s.nextInt();
+        System.out.println("how many times u need to multiply");
+        int hw= s.nextInt();
+        int mul;
+        
+        for(int i=1;i<=hw;i++)
+        {
+            mul=no*i;
+            System.out.println(""+no+"*"+i+"="+mul);
+        }
+        return no;
+	}
+    public static void main(String[] args) {
+        Multiplication m = new Multiplication();
+        m.mul();
+        System.out.println("the noo");
+        
+    }
+    
+}
